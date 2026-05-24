60796ca4cbdd37b7b291a39c6a6b78e5fced2a86
tranvo-dev <tranvo.developer@gmail.com>
2026-05-24 08:30:54 +0700

    init project

---
+++ service/course-enrollment-service/src/main/java/com/tranvo_dev/course_enrollment_service/CourseEnrollmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tranvo_dev.course_enrollment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseEnrollmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseEnrollmentServiceApplication.class, args);
+	}
+
+}
---
+++ service/course-enrollment-service/src/test/java/com/tranvo_dev/course_enrollment_service/CourseEnrollmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tranvo_dev.course_enrollment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseEnrollmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
